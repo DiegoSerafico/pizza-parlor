2c9f7c6aabc375e67d984760af19107c124b9c06
Diego Serafico <dseraficohernandez@gmail.com>
2021-08-27 10:42:42 -0700

    add first passed test for calculating cost

---
+++ README.md
@@ -0,0 +1,7 @@
+Describe calculateCost()
+
+Test: "It will give the cost for the size of pizza"
+Code: 
+let myPizza = new Pizza("small", "pepperoni");
+myPizza.calculateCost();
+Expected Output: 9.99
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-function PizzaOrder() {
-  
-}
+const availableToppings = {pepperoni: 1.0, sausage: 1.0, bacon: 1.0, pinneaple: 0.5, extra_cheese: 2.0, jalapenos: 0.75, salami: 1.0, olives: 0.5, parmesan: 0.5};
 
-PizzaOrder.prototype.cost = function() {
-  
-}
-
-let availableToppings = {pepperoni: 1.0, sausage: 1.0, bacon: 1.0, pinneaple: 0.5, extra cheese: 2.0, jalapenos: 0.75, salami: 1.0, olives: 0.5, parmesan: 0.5};
-
-let availableSizes = {small: 9.99, medium: 12.99, large: 16.99};
+const availableSizes = {small: 9.99, medium: 12.99, large: 16.99};
 
 function Pizza (size, toppings) {
   this.toppings = toppings;
   this.size = size;
 }
+
+Pizza.prototype.calculateCost = function() {
+  let cost = 0.0;
+
+  cost += availableSizes[this.size];
+  return cost;
+}
+
+let myPizza = new Pizza("small", "pepperoni");
