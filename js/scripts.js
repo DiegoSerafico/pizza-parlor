eb36304d26ee747c4e2cee17b7e3b7aaf73dff60
Diego Serafico <dseraficohernandez@gmail.com>
2021-08-27 14:13:44 -0700

    add form without functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const availableToppings = {pepperoni: 1.0, sausage: 1.0, bacon: 1.0, pinneaple: 0.5, extra_cheese: 2.0, jalapenos: 0.75, salami: 1.0, olives: 0.5, parmesan: 0.5};
+const availableToppings = {pepperoni: 1.0, sausage: 1.0, bacon: 1.0, pineapple: 0.5, extra_cheese: 2.0, jalapenos: 0.75, salami: 1.0, olives: 0.5, parmesan: 0.5};
 
 const availableSizes = {small: 9.99, medium: 12.99, large: 16.99};
 
